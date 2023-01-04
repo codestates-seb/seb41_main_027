261c24f5e67f73cea5d11a3843ecb726e6925c6e
Koreant_051 <dg.bomikang@gmail.com>
2023-01-04 19:02:24 +0900

    💄 style : 전역 스타일 셋업
    - styled-reset 을 사용하여 기존의 css 를 초기화하기 위해
    styled-comonents 와 styled-reset 설치함
    - npm i styled-components styled-reset
    - .prettierrc, .eslintrc 기본 세팅만 완료

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import GlobalStyle from '../src/styles/GlobalStyle'
+
+function App() {
+  return (
+    <section className="App">
+      <reset />
+      <GlobalStyle />
+      <h1 className="test">🫡 Set up 🫡</h1>
+      {/* 주석 샘플 나중에 다쓰고 날릴게요🥹 */}
+    </section>
+  )
+}
+
+export default App
---
+++ client/src/styles/GlobalStyle.js
@@ -0,0 +1,64 @@
+// global style 적용
+import { createGlobalStyle } from 'styled-components'
+import reset from 'styled-reset'
+
+// 글로벌 스타일 만들기, reset 사용해서 스타일 초기화하기
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  /* 필요한 부분 추가해주셔요 🥹 */
+  .test { font-size: 2rem; margin: 32px} /* set up test 용 날리시면 됩니다 */
+  
+  body{
+    margin: 0;
+    padding: 0;
+    min-height: 100vh;
+    background-color: #ffffff;
+    color: #232629;
+    font-family: "Roboto",system-ui,BlinkMacSystemFont,"Segoe UI","Helvetica Neue",Arial,sans-serif; 
+  }
+
+  a {
+    text-decoration: none;
+    outline: none;
+    font-size: 100%;
+    color: inherit;
+  }
+
+  input, button {
+    background-color: transparent;
+    border: none;
+    outline: none;
+  }
+
+  h1, h2, h3, h4, h5, h6{
+    font-weight: normal;
+    margin: 0;
+    font-size: 100%;
+  }
+
+  ul {
+    margin: 0;
+    padding: 0;
+  }
+
+  ol, ul, li {
+    list-style: none;
+  }
+
+  img {
+    display: block;
+    width: 100%;
+    height: 100%;
+  }
+
+  button {
+    padding: 0;
+    background-color: transparent;
+    font-size: 100%;
+    border: none;
+    cursor: pointer;
+  }
+
+`
+
+export default GlobalStyle
