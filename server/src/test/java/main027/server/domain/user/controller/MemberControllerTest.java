c5fd73dac29b96a76c71fc99fcd97a090a4db2fb
wonyongg <kakaohwy@gmail.com>
2023-01-05 16:28:54 +0900

    chore : userController -> memberController로 수정

---
+++ server/src/test/java/main027/server/domain/user/controller/MemberControllerTest.java
@@ -0,0 +1,26 @@
+package main027.server.domain.user.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Transactional
+@SpringBootTest
+@AutoConfigureMockMvc
+class MemberControllerTest {
+
+    @Test
+    void postUser() {
+    }
+
+    @Test
+    void patchUser() {
+    }
+
+    @Test
+    void deleteUser() {
+    }
+}
