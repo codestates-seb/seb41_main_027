60517da8f46b25bd8499cdaa670e98e874dc638f
tlskals <111082777+tlskals@users.noreply.github.com>
2023-01-05 15:54:35 +0900

    🔨 chore : Place Entity 의존 관계 수정
    Place Entity Base Time 수정

--- server/src/main/java/main027/server/domain/place/controller/PlaceController.java
+++ server/src/main/java/main027/server/domain/place/controller/PlaceController.java
@@ -1,11 +1,36 @@
 package main027.server.domain.place.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/places")
 @Validated
 public class PlaceController {
+
+    @PostMapping
+    public ResponseEntity postPlace() {
+        return ResponseEntity.created(null).build();
+    }
+
+    @PatchMapping("/{placeId}")
+    public ResponseEntity patchPlace() {
+        return ResponseEntity.ok(null);
+    }
+
+    @GetMapping("/{placeId}")
+    public ResponseEntity getPlace() {
+        return ResponseEntity.ok(null);
+    }
+
+    @GetMapping
+    public ResponseEntity getPlaces() {
+        return ResponseEntity.ok(null);
+    }
+
+    @DeleteMapping("/{placeId}")
+    public ResponseEntity deletePlace() {
+        return ResponseEntity.noContent().build();
+    }
 }
