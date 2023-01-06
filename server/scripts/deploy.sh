28abae4e0c1c4b80d0dda516c3b3f22ac0262e45
wonyongg <kakaohwy@gmail.com>
2023-01-06 22:38:48 +0900

    docs : deploy.sh 수정3

--- server/scripts/deploy.sh
+++ server/scripts/deploy.sh
@@ -23,9 +23,11 @@ else
   sleep 5
 fi
 
+#DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
+#echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/seb41_main_027/deploy.log
+#sudo nohup java -jar $DEPLOY_JAR >> /home/seb41_main_027/deploy.log 2>/home/seb41_main_027/deploy_err.log &
 
-DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/seb41_main_027/deploy.log
-sudo nohup java -jar $DEPLOY_JAR >> /home/seb41_main_027/deploy.log 2>/home/seb41_main_027/deploy_err.log &
-
-#
+DEPLOY_JAR=$JAR_NAME
+    echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/seb41_main_027/deploy.log
+    cd "> $DEPLOY_PATH"
+    sudo nohup java -jar $DEPLOY_JAR >> /home/ubuntu/deploy.log 2>/home/ubuntu/seb41_main_027/deploy_err.log &
