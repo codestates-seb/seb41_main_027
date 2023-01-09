bd4e54565430566473b001fc10751e0e91e49102
wonyongg <kakaohwy@gmail.com>
2023-01-09 19:35:06 +0900

    chore : Member 엔티티 컨트롤러 dto 수정

--- server/src/main/java/main027/server/domain/member/controller/MemberController.java
+++ server/src/main/java/main027/server/domain/member/controller/MemberController.java
@@ -1,32 +1,51 @@
 package main027.server.domain.member.controller;
 
 import main027.server.domain.member.dto.MemberDto;
+import main027.server.domain.member.entity.Member;
+import main027.server.domain.member.mapper.MemberMapper;
+import main027.server.domain.member.service.MemberService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Positive;
 import java.net.URI;
 import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("/members")
+@Validated
 public class MemberController {
+    private final MemberService memberService;
+    private final MemberMapper mapper;
+
+    public MemberController(MemberService memberService, MemberMapper mapper) {
+        this.memberService = memberService;
+        this.mapper = mapper;
+    }
+
     @PostMapping
-    public ResponseEntity postMember() {
-        return ResponseEntity.created(URI.create("/members/1")).build();
+    public ResponseEntity postMember(@Valid @RequestBody MemberDto.Post requestBody) {
+        Member createdMember = memberService.createMember(mapper.memberPostDtoToMember(requestBody));
+
+        return new ResponseEntity<>(mapper.memberToMemberResponseDto(createdMember), HttpStatus.CREATED);
     }
 
-    @PatchMapping("/{member-id}")
-    public ResponseEntity patchMember() {
-        MemberDto.Response response = new MemberDto.Response(1L,
-                                                             "hgd@gmail.com",
-                                                             "홍길동",
-                                                             LocalDateTime.now(),
-                                                             LocalDateTime.now());
-        return ResponseEntity.ok(response);
+    @PatchMapping("/{memberId}")
+    public ResponseEntity patchMember(@Valid @RequestBody MemberDto.Patch requestBody,
+                                      @Positive @PathVariable("memberId") Long memberId) {
+        requestBody.setId(memberId);
+        Member updatedMember = memberService.updateMember(mapper.memberPatchDtoToMember(requestBody));
+
+        return new ResponseEntity<>(mapper.memberToMemberResponseDto(updatedMember), HttpStatus.OK);
     }
 
-    @DeleteMapping("/{member-id}")
-    public ResponseEntity deleteMember() {
-        return ResponseEntity.noContent().build();
+    @DeleteMapping("/{memberId}")
+    public ResponseEntity deleteMember(@Positive @PathVariable("memberId") Long memberId) {
+        memberService.deleteMember(memberId);
+
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 }
