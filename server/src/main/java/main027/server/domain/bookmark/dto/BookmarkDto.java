d5b3c2ead06b77b09616733b283eec1a0a1a8d51
itsowavy <qndldtmsl3138@gmail.com>
2023-01-10 15:40:54 +0900

    🔨 chore : Bookmark 코드 수정

--- server/src/main/java/main027/server/domain/bookmark/dto/BookmarkDto.java
+++ server/src/main/java/main027/server/domain/bookmark/dto/BookmarkDto.java
@@ -1,9 +1,14 @@
 package main027.server.domain.bookmark.dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import javax.validation.constraints.NotBlank;
 
 public class BookmarkDto {
 
+    @NoArgsConstructor
+    @Getter
     public static class Post {
 
         @NotBlank
--- server/src/main/java/main027/server/domain/bookmark/repository/BookmarkRepository.java
+++ server/src/main/java/main027/server/domain/bookmark/repository/BookmarkRepository.java
@@ -2,15 +2,15 @@ package main027.server.domain.bookmark.repository;
 
 import main027.server.domain.bookmark.entity.Bookmark;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
+
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
 
-    @Query("select b from Bookmark b where b.member.memberId = :memberId and b.place.placeId = :palceId")
+    @Query("select b from Bookmark b where b.member.memberId = :memberId and b.place.placeId = :placeId")
     Optional<Bookmark> checkBookmarked(@Param("memberId") Long memberId, @Param("placeId") Long placeId);
 
     // @Modifying
