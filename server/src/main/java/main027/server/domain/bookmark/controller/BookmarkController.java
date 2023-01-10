2c3f7c37b307268b791557297d752ecea59bd58b
itsowavy <qndldtmsl3138@gmail.com>
2023-01-11 00:48:26 +0900

    ✨ feat : Bookmark Post 구현

--- server/src/main/java/main027/server/domain/bookmark/controller/BookmarkController.java
+++ server/src/main/java/main027/server/domain/bookmark/controller/BookmarkController.java
@@ -9,10 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/bookmarks")
@@ -23,9 +20,9 @@ public class BookmarkController {
     private final BookmarkMapper mapper;
 
     @PostMapping
-    public ResponseEntity post(@Validated BookmarkDto.Post postDto) {
+    public ResponseEntity post(@Validated @RequestBody BookmarkDto.Post postDto) {
         Bookmark bookmark = mapper.PostToEntity(postDto);
-
-        return new ResponseEntity(HttpStatus.OK);
+        Boolean finalBookmarkStatus = bookmarkService.changeBookmarkStatus(bookmark);
+        return new ResponseEntity(finalBookmarkStatus, HttpStatus.OK);
     }
 }
