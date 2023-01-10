e8880180a1e452e0dc3b1319c752280ecf487718
itsowavy <qndldtmsl3138@gmail.com>
2023-01-10 12:01:12 +0900

    ✨ feat : Bookmark 서비스, 리포지토리 작성

--- server/src/main/java/main027/server/domain/bookmark/repository/BookmarkRepository.java
+++ server/src/main/java/main027/server/domain/bookmark/repository/BookmarkRepository.java
@@ -2,6 +2,18 @@ package main027.server.domain.bookmark.repository;
 
 import main027.server.domain.bookmark.entity.Bookmark;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
+
+    @Query("select b from Bookmark b where b.member.memberId = :memberId and b.place.placeId = :palceId")
+    Optional<Bookmark> checkBookmarked(@Param("memberId") Long memberId, @Param("placeId") Long placeId);
+
+    // @Modifying
+    // @Query("delete b from Bookmark b where b.member.memberId = :memberId and b.place.placeId = :palceId")
+    // int removeBookmark(@Param("memberId") Long memberId, @Param("placeId") Long placeId);
 }
