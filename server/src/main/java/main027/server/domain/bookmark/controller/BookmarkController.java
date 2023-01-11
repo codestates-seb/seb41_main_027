699b9a06412500913340d0d5692bfb05c9a401ba
itsowavy <qndldtmsl3138@gmail.com>
2023-01-11 11:12:19 +0900

    ✨ feat : BookMark Dto 관련 코드 추가

--- server/src/main/java/main027/server/domain/bookmark/controller/BookmarkController.java
+++ server/src/main/java/main027/server/domain/bookmark/controller/BookmarkController.java
@@ -5,11 +5,16 @@ import main027.server.domain.bookmark.dto.BookmarkDto;
 import main027.server.domain.bookmark.entity.Bookmark;
 import main027.server.domain.bookmark.mapper.BookmarkMapper;
 import main027.server.domain.bookmark.service.BookmarkService;
+import main027.server.domain.place.entity.Place;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/bookmarks")
@@ -24,5 +29,16 @@ public class BookmarkController {
         Bookmark bookmark = mapper.PostToEntity(postDto);
         Boolean finalBookmarkStatus = bookmarkService.changeBookmarkStatus(bookmark);
         return new ResponseEntity(finalBookmarkStatus, HttpStatus.OK);
+    }
+
+    @GetMapping("/{memberId}")
+    public ResponseEntity getList(@PathVariable Long memberId,
+                                  @RequestParam(defaultValue = "1") int page) {
+
+        Page<Place> pagingList = bookmarkService.findPlaceMemberBookmarked(memberId, PageRequest.of(page-1, 10));
+
+        BookmarkDto.Response response = mapper.pageToList(pagingList);
+
+        return new ResponseEntity(response, HttpStatus.OK);
     }
 }
