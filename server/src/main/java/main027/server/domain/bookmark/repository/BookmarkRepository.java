9506b4c5945c9cff40feabedffbd154f7231ef4a
itsowavy <qndldtmsl3138@gmail.com>
2023-01-11 11:11:02 +0900

    ✨ feat : Bookmark 조회 service repository 구현

--- server/src/main/java/main027/server/domain/bookmark/repository/BookmarkRepository.java
+++ server/src/main/java/main027/server/domain/bookmark/repository/BookmarkRepository.java
@@ -1,10 +1,14 @@
 package main027.server.domain.bookmark.repository;
 
 import main027.server.domain.bookmark.entity.Bookmark;
+import main027.server.domain.place.entity.Place;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 
@@ -13,6 +17,8 @@ public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
     @Query("select b from Bookmark b where b.member.memberId = :memberId and b.place.placeId = :placeId")
     Optional<Bookmark> checkBookmarked(@Param("memberId") Long memberId, @Param("placeId") Long placeId);
 
+    @Query("select b.place from Bookmark b where b.member.memberId = :memberId")
+    Page<Place> findBookmark(@Param("memberId") Long memberId, Pageable pageable);
     // @Modifying
     // @Query("delete b from Bookmark b where b.member.memberId = :memberId and b.place.placeId = :palceId")
     // int removeBookmark(@Param("memberId") Long memberId, @Param("placeId") Long placeId);
--- server/src/main/java/main027/server/domain/bookmark/service/BookmarkService.java
+++ server/src/main/java/main027/server/domain/bookmark/service/BookmarkService.java
@@ -4,10 +4,14 @@ import lombok.RequiredArgsConstructor;
 import main027.server.domain.bookmark.entity.Bookmark;
 import main027.server.domain.bookmark.repository.BookmarkRepository;
 import main027.server.domain.member.service.MemberService;
+import main027.server.domain.place.entity.Place;
 import main027.server.domain.place.service.PlaceService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -18,6 +22,7 @@ public class BookmarkService {
     private final BookmarkRepository bookmarkRepository;
     private final MemberService memberService;
     private final PlaceService placeService;
+
     /**
      * @return 북마킹이 되어 있지 않았고 해당 로직을 통해 북마킹이 되었다면 true 리턴 <br>
      *         북마킹이 되어 있었고 해당 로직을 통해 북마킹이 해제되었다면 false 리턴
@@ -44,6 +49,13 @@ public class BookmarkService {
             return true;
         }
 
+    }
+
+    public Page<Place> findPlaceMemberBookmarked(Long memberId, Pageable pageable) {
+        // 존재하는 memberId인지 검증
+        memberService.findVerifiedMember(memberId);
+
+        return bookmarkRepository.findBookmark(memberId, pageable);
     }
 
 }
