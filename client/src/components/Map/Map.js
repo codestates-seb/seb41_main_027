7786d12fded73dd527ad591b84e5b7ad395d0cf0
Koreant_051 <dg.bomikang@gmail.com>
2023-01-13 00:53:45 +0900

    💄 style : SiteInfoCard

--- client/src/components/Map/Map.js
+++ client/src/components/Map/Map.js
@@ -3,18 +3,41 @@ import { MapMarker, Map } from 'react-kakao-maps-sdk'
 import { useRef, useMemo, useState, useEffect } from 'react'
 
 import SearchBar from './SearchBar/SearchBar'
+import SiteInfoCard from './SiteInfoCard/SiteInfoCard'
 
 const Container = styled.section`
   position: relative;
   z-index: 1000;
   overflow: hidden;
-  /* padding: 32px; // Demo Position 🫡 */
   box-sizing: border-box;
   height: calc(100% - 88px);
   border-radius: 32px 0px 0px 0px;
   box-shadow: -8px -4px 30px rgba(0, 129, 76, 0.6);
   /* box-shadow: -8px -4px 30px rgba(0, 129, 76, 0.4); */
   background-color: #fff;
+
+  .site-list {
+    position: absolute;
+    z-index: 1500;
+    // Demo Position 🫡
+    top: 140px;
+    right: 32px;
+    width: inherit;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    gap: 8px;
+    overflow-y: auto;
+    // 🚧 scroll Cut-off shadow 처리용
+    margin: 0px -32px;
+    padding: 0px 32px;
+    -ms-overflow-style: none; // IE
+    &::-webkit-scrollbar {
+      display: none !important; // etc
+    }
+    border-radius: 12px;
+  }
 `
 
 const { kakao } = window
@@ -43,11 +66,31 @@ const Main = () => {
   return (
     <Container>
       <SearchBar />
+      <div className="site-list">
+        <SiteInfoCard />
+        <SiteInfoCard />
+        <SiteInfoCard />
+        <SiteInfoCard />
+        <SiteInfoCard />
+        <SiteInfoCard />
+        <SiteInfoCard />
+        <SiteInfoCard />
+        <SiteInfoCard />
+        <SiteInfoCard />
+        <SiteInfoCard />
+        <SiteInfoCard />
+        <SiteInfoCard />
+        <SiteInfoCard />
+        <SiteInfoCard />
+        <SiteInfoCard />
+        <SiteInfoCard />
+        <SiteInfoCard />
+      </div>
       <Map // 지도를 표시할 Container
         center={{ lat: 37.542668, lng: 126.976395 }}
         style={{
           width: '100%',
-          height: '450px',
+          height: '100%',
         }}
         level={6} // 지도의 확대 레벨
         ref={mapRef}
