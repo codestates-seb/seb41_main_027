e651fd1c984278441c22e7d92e75a0ee4dc80916
Double.Pos <88361186+DubblePos@users.noreply.github.com>
2023-01-12 10:36:23 +0900

    ✨ feat : maps (#103)
    * ✨ feat : map bound
    
    * 🔨 chore : 오타 수정
    
    Co-authored-by: Koreant_051 <dg.bomikang@gmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,13 @@ import styled from 'styled-components'
 import GlobalStyle from '../src/styles/GlobalStyle'
 import { Reset } from 'styled-reset'
 import { Routes, Route } from 'react-router-dom'
+
 import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
-import ScrollToTop from './utils/ScrollToTop'
-
-import Home from './pages/Home'
-import Place from './pages/Place'
-import Mypage from './pages/Mypage'
-import Aboutus from './pages/AboutUs'
-import SignUp from './pages/SignUp'
-import SignIn from './pages/SignIn'
-import { useEffect } from 'react'
+import Nav from './components/Nav/Nav'
+import Map from './components/Map/Map'
+import Header from './components/Header/Header'
 
 const Main = styled.main`
   display: flex;
@@ -23,31 +18,21 @@ const Main = styled.main`
   box-sizing: border-box;
 `
 
-const StyleToastContainer = styled(ToastContainer)`
-  z-index: 9500;
+const Wrapper = styled.section`
+  width: 100%;
 `
 
 function App() {
-  // 전역 상태 추가되면 나중에 뺄게요.
-  useEffect(() => {
-    toast('App reload', { autoClose: 500 })
-  }, [])
-
   return (
     <section className="App">
       <Reset />
       <GlobalStyle />
-      <ScrollToTop />
-      <StyleToastContainer position="top-center" pauseOnFocusLoss draggable pauseOnHover />
       <Main>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/place" element={<Place />} />
-          <Route path="/mypage" element={<Mypage />} />
-          <Route path="/aboutus" element={<Aboutus />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/signin" element={<SignIn />} />
-        </Routes>
+        <Nav />
+        <Wrapper>
+          <Header />
+          <Map />
+        </Wrapper>
       </Main>
       {/* 주석 샘플 다쓰고 나중에 날릴게요🥹 */}
     </section>
--- client/src/components/Map/Map.js
+++ client/src/components/Map/Map.js
@@ -1,4 +1,6 @@
 import styled from 'styled-components'
+import { MapMarker, Map } from 'react-kakao-maps-sdk'
+import { useRef, useMemo, useState, useEffect } from 'react'
 
 import SearchBar from './SearchBar/SearchBar'
 
@@ -12,13 +14,54 @@ const Container = styled.section`
   box-shadow: -8px -4px 30px rgba(0, 129, 76, 0.4);
   background-color: #fff;
 `
+const { kakao } = window
 
-const Map = () => {
+const Main = () => {
+  const mapRef = useRef()
+  const [points, setPoints] = useState([
+    { lat: 33.452278, lng: 126.567803 },
+    { lat: 33.452671, lng: 126.574792 },
+    { lat: 33.451744, lng: 126.572441 },
+  ])
+
+  const bounds = useMemo(() => {
+    const bounds = new kakao.maps.LatLngBounds()
+
+    points.forEach(point => {
+      bounds.extend(new kakao.maps.LatLng(point.lat, point.lng))
+    })
+    return bounds
+  }, [points])
+  useEffect(() => {
+    const map = mapRef.current
+    if (map) map.setBounds(bounds)
+  }, [points])
+
   return (
     <Container>
       <SearchBar />
+      <Map // 지도를 표시할 Container
+        center={{ lat: 37.542668, lng: 126.976395 }}
+        style={{
+          width: '100%',
+          height: '450px',
+        }}
+        level={6} // 지도의 확대 레벨
+        ref={mapRef}
+      >
+        {points.map(point => (
+          <MapMarker key={`${point.lat}-${point.lng}`} position={point} />
+        ))}
+      </Map>
+      <button
+        onClick={() => {
+          const map = mapRef.current
+          if (map) map.setBounds(bounds)
+        }}
+      >
+        지도 범위 재설정 하기
+      </button>
     </Container>
   )
 }
-
-export default Map
+export default Main
