09a86cd9500cec1e7a14b7a082820770b532d6b4
gidoldory <gidol79@gmail.com>
2023-01-13 15:28:19 +0900

    ✨ feat : api timeout 설정 분리, getFetch hook 추가

--- client/src/api/bookmark.js
+++ client/src/api/bookmark.js
@@ -1,24 +1,24 @@
 import axios from 'axios'
 
 const API_BOOKMARK_ENDPOINT = process.env.REACT_APP_API + process.env.REACT_APP_API_BOOKMARK_ENDPOINT
-const API_CONNECT_TIMEOUT = 2000
+const API_CONNECT_TIMEOUT = process.env.REACT_APP_API_CONNECT_TIMEOUT
 
 // create
-export const bookmarkCreate = body => {
+export const createBookmark = body => {
   axios.post(API_BOOKMARK_ENDPOINT, body, {
     timeout: API_CONNECT_TIMEOUT,
   })
 }
 
 // update
-export const bookmarkUpdate = body => {
+export const updateBookmark = body => {
   axios.patch(API_BOOKMARK_ENDPOINT, body, {
     timeout: API_CONNECT_TIMEOUT,
   })
 }
 
 // delete
-export const bookmarkDelete = () => {
+export const deleteBookmark = () => {
   axios.delete(API_BOOKMARK_ENDPOINT, {
     timeout: API_CONNECT_TIMEOUT,
   })
