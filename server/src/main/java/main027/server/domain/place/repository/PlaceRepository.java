e8438fe011d576ca06fac737c0a02166b69ca18a
gidoldory <gidol79@gmail.com>
2023-01-13 18:25:08 +0900

    ✅ test: frontend 복원

--- server/src/main/java/main027/server/domain/place/repository/PlaceRepository.java
+++
@@ -1,7 +0,0 @@
-package main027.server.domain.place.repository;
-
-import main027.server.domain.place.entity.Place;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlaceRepository extends JpaRepository<Place, Long> {
-}
