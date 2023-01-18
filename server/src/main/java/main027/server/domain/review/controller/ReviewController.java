f2c788ff975d982daac8a1c0022a588056e6427d
itsowavy <qndldtmsl3138@gmail.com>
2023-01-18 18:55:47 +0900

    🎨 refactor : bookmark, review 컨트롤러 refactor

--- server/src/main/java/main027/server/domain/review/controller/ReviewController.java
+++ server/src/main/java/main027/server/domain/review/controller/ReviewController.java
@@ -2,7 +2,6 @@ package main027.server.domain.review.controller;
 
 import lombok.RequiredArgsConstructor;
 import main027.server.domain.review.dto.ReviewDto;
-import main027.server.domain.review.entity.Review;
 import main027.server.domain.review.mapper.ReviewMapper;
 import main027.server.domain.review.service.ReviewService;
 import main027.server.domain.review.verifier.ReviewVerifier;
@@ -27,21 +26,21 @@ public class ReviewController {
      */
     @PostMapping
     public ResponseEntity post(@Validated @RequestBody ReviewDto.Post postDto) {
-        Review review = mapper.PostToEntity(postDto);
-        ReviewDto.Response response = mapper.entityToResponse(reviewService.save(review));
+        ReviewDto.Response response = mapper.entityToResponse(reviewService.save(mapper.PostToEntity(postDto)));
 
         return new ResponseEntity(response, HttpStatus.CREATED);
     }
 
     /**
      * 장소에 등록되어 있는 리뷰 목록을 페이징처리로 가져오는 컨트롤러
+     *
      * @param placeId 리뷰 목록을 가져올 장소의 id
-     * @param page 가져오고 싶은 페이지 (default: 1)
+     * @param page    가져오고 싶은 페이지 (default: 1)
      * @return {@link ReviewDto.ListResponse}
      */
     @GetMapping("/{placeId}")
     public ResponseEntity getPlaceReviews(@PathVariable Long placeId,
-                                          @RequestParam (defaultValue = "1") int page) {
+                                          @RequestParam(defaultValue = "1") int page) {
         Pageable pageable = PageRequest.of(page - 1, 10);
 
         ReviewDto.ListResponse response = mapper.pageToList(reviewService.findReviews(placeId, pageable));
