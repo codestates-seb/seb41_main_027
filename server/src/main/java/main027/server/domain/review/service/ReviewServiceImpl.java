38f1a0405e3bd27f144210a315e92f98384987dc
itsowavy <qndldtmsl3138@gmail.com>
2023-01-18 19:00:35 +0900

    🔨 chore : Review @TimeTrace 추가

--- server/src/main/java/main027/server/domain/review/service/ReviewServiceImpl.java
+++ server/src/main/java/main027/server/domain/review/service/ReviewServiceImpl.java
@@ -6,6 +6,7 @@ import main027.server.domain.place.verifier.PlaceVerifier;
 import main027.server.domain.review.entity.Review;
 import main027.server.domain.review.repository.ReviewRepository;
 import main027.server.domain.review.verifier.EmojiVerifier;
+import main027.server.global.aop.logging.annotation.TimeTrace;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -22,6 +23,7 @@ public class ReviewServiceImpl implements ReviewService{
     /**
      *  리뷰의 Emoji, Place, Member가 유효한지 검사 후 저장
      */
+    @TimeTrace
     public Review save(Review review) {
         emojiVerifier.findVerifiedEmoji(review.getEmoji().getEmojiId());
         memberVerifier.findVerifiedMember(review.getMember().getMemberId());
@@ -35,6 +37,7 @@ public class ReviewServiceImpl implements ReviewService{
      *
      * @return 페이징 처리 된 Review
      */
+    @TimeTrace
     @Override
     public Page<Review> findReviews(Long placeId, Pageable pageable) {
         placeVerifier.findVerifiedPlace(placeId);
