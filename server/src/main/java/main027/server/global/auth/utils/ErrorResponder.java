162be8aee498c9900b5d026e7072cbfd31fbdd9a
wonyongg <kakaohwy@gmail.com>
2023-01-22 00:50:13 +0900

    chore : ErrorResponder 수정

--- server/src/main/java/main027/server/global/auth/utils/ErrorResponder.java
+++ server/src/main/java/main027/server/global/auth/utils/ErrorResponder.java
@@ -17,8 +17,13 @@ public class ErrorResponder {
     public static void sendErrorResponse(HttpServletRequest request, HttpServletResponse response, HttpStatus status) throws IOException {
         Gson gson = new Gson();
         Exception exception = (Exception) request.getAttribute("exception");
+        ErrorResponse errorResponse;
 
-        ErrorResponse errorResponse = ErrorResponse.of(status, exception.getMessage());
+        if (exception.getMessage() != null) {
+            errorResponse = ErrorResponse.of(status, exception.getMessage());
+        } else {
+            errorResponse = ErrorResponse.of(status);
+        }
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         response.setStatus(status.value());
         response.getWriter().write(gson.toJson(errorResponse, ErrorResponse.class));
