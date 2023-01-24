dd358d6816cbe7a127c399288072289a9bb23a92
Koreant_051 <dg.bomikang@gmail.com>
2023-01-25 00:46:47 +0900

    🔨 chore : 회원가입 및 로그인 유효성 테스트
    - 닉네임 및 패스워드 유효성 정책 수정 필요함

--- client/src/components/Sign/Register.js
+++ client/src/components/Sign/Register.js
@@ -1,11 +1,13 @@
 import axios from '../../api/sign'
-import { useRef, useState, useEffect } from 'react'
+import { useRef, useState, useEffect, useCallback } from 'react'
 import { Link } from 'react-router-dom'
 
 // import '../Sign/Register.css'
 import styled from 'styled-components'
-import { faCircleCheck, faCircleXmark, faInfoCircle } from '@fortawesome/free-solid-svg-icons'
+import { faCircleCheck, faCircleXmark } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+// import { vaildEmail, createMember } from '../../api/member'
 
 //💄 Demo Styles --------------------------------
 const Container = styled.div`
@@ -45,7 +47,7 @@ const Container = styled.div`
         gap: 8px;
         font-weight: 300;
         font-size: 12px;
-        & a {
+        & > a {
           color: #000;
           padding-left: 8px;
           font-weight: 600;
@@ -115,12 +117,11 @@ const Container = styled.div`
 
   // 유효성 에러메세지 비노출시 demo
   .offscreen {
-    position: absolute;
-    left: -9999px;
+    display: none;
   }
   // 유효성 에러메세지 노출시 demo
   .instructions {
-    position: relative;
+    /* position: relative; */
     width: 100%;
     padding: 0 8px 16px;
     box-sizing: border-box;
@@ -160,21 +161,17 @@ const Container = styled.div`
   }
 `
 
-//💄 Demo Regex ---------------------------------
-const REGISTER_URL = 'http://localhost:3001/signup'
+// 🤖  Regex set ----------------------------------------
+const REGISTER_URL = 'http://52.78.152.135:8080/members'
 
 const EMAIL_REGEX = new RegExp('[a-z0-9]+@[a-z]+.[a-z]{2,3}')
-const USER_REGEX = /^(?=.*[a-z0-9가-힣])[a-z0-9가-힣]{2,16}$/
+const USERNAME_REGEX = /^(?=.*[a-z0-9가-힣])[a-z0-9가-힣]{2,16}$/
 // 2자 이상 16자 이하, 영어 또는 숫자 또는 한글로 구성
-// *한글 초성 및 모음은 허가하지 않음
+// * 한글 초성 및 모음은 허용하지 않음
 const PWD_REGEX = /(?=.*[0-9])(?=.*[a-z]).{8,20}/
 // 8자 이상 20자 이하, 숫자와 영문의 조합으로 구성
 
-// 🚧 더미 추후 날리겠습니다. ----------------------------------------
-// const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/
-// const EMAIL_REGEX = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/
-// const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/
-
+// 🤖 valid set ----------------------------------------
 const Register = () => {
   const userRef = useRef()
   const errRef = useRef()
@@ -182,12 +179,14 @@ const Register = () => {
   const [email, setEmail] = useState('')
   const [validEmail, setValidEmail] = useState(false)
   const [emailFocus, setEmailFocus] = useState(false)
+  const [emailMessage, setEmailMessage] = useState('')
 
-  const [username, setUsername] = useState('')
-  const [validUsername, setValidUsername] = useState(false)
+  const [nickName, setNickname] = useState('')
+  const [validnickName, setValidnickName] = useState(false)
   const [userFocus, setUserFocus] = useState(false)
+  const [nickNameMessage, setNickNameMessage] = useState('')
 
-  const [pwd, setPwd] = useState('')
+  const [password, setPassword] = useState('')
   const [validPwd, setValidPwd] = useState(false)
   const [pwdFocus, setPwdFocus] = useState(false)
 
@@ -203,37 +202,49 @@ const Register = () => {
   }, [])
 
   useEffect(() => {
-    setValidEmail(EMAIL_REGEX.test(email))
+    const vaildEmail = EMAIL_REGEX.test(email)
+    if (vaildEmail) {
+      // printValue(email, 'email')
+    }
+    setValidEmail(vaildEmail)
   }, [email])
 
   useEffect(() => {
-    setValidUsername(USER_REGEX.test(username))
-  }, [username])
+    const vaildNickName = USERNAME_REGEX.test(nickName)
+    if (vaildNickName) {
+      // printValue(nickName, 'nickname')
+    }
+    setValidnickName(USERNAME_REGEX.test(nickName))
+  }, [nickName])
 
   useEffect(() => {
-    setValidPwd(PWD_REGEX.test(pwd))
-    setValidMatch(pwd === matchPwd)
-  }, [pwd, matchPwd])
+    setValidPwd(PWD_REGEX.test(password))
+    setValidMatch(password === matchPwd)
+  }, [password, matchPwd])
 
   useEffect(() => {
     setErrMsg('')
-  }, [email, username, pwd, matchPwd])
+  }, [email, nickName, password, matchPwd])
 
+  // 🤖 회원가입 버튼 핸들링 ----------------------------------------
   const handleSubmit = async e => {
     e.preventDefault()
     // 버튼 활성화 되었을 경우 (js hack)
     const v1 = EMAIL_REGEX.test(email)
-    const v2 = USER_REGEX.test(username)
-    const v3 = PWD_REGEX.test(pwd)
+    const v2 = USERNAME_REGEX.test(nickName)
+    const v3 = PWD_REGEX.test(password)
     if (!v1 || !v2 || !v3) {
-      setErrMsg('입력된 정보를 다시 확인해주세요.')
+      setErrMsg('입력된 정보들을 다시 확인해주세요.')
       return
     }
     try {
-      const response = await axios.post(REGISTER_URL, JSON.stringify({ email, username, pwd }), {
-        headers: { 'Content-Type': 'application/json' },
-        withCredentials: true,
-      })
+      const response = await axios.post(
+        REGISTER_URL,
+        { email, nickName, password },
+        {
+          headers: { 'Content-Type': 'application/json', withCredentials: true },
+        },
+      )
       console.log(response?.data)
       console.log(response?.accessToken)
       console.log(JSON.stringify(response))
@@ -241,21 +252,20 @@ const Register = () => {
       // 상태 및 인풋 날리기 clean!
       // 입력을 위한 대한 속성 값 필요
       setEmail('')
-      setUsername('')
-      setPwd('')
+      setNickname('')
+      setPassword('')
       setMatchPwd('')
     } catch (err) {
       if (!err?.response) {
-        setErrMsg('서버의 응답이 없습니다. 새로고침을 해주세요.')
-      } else if (err.response?.status === 404) {
-        setErrMsg('이미 사용중인 닉네임입니다. 닉네임을 변경해주세요.')
+        setErrMsg('서버의 응답이 없습니다. 새로고침 후 다시 시도해주세요.😭')
+      } else if (err.response?.status === 409) {
+        setErrMsg('이미 사용중인 닉네임과 비밀번호입니다. 변경해주세요.')
       } else {
         setErrMsg('회원 가입에 실패했습니다.😭 다시 시도해주세요.')
       }
       errRef.current.focus()
     }
   }
-
   return (
     <Container>
       {success ? (
@@ -283,6 +293,10 @@ const Register = () => {
                 ref={userRef}
                 autoComplete="off"
                 onChange={e => setEmail(e.target.value)}
+                // 멘토님 Q. 지금 비효율 적임 만약 새로 짠다면?
+                // 기존로직: onChange > setEmail > email changed > email 종속 useEffect > 유효성 체크 > 유효성 결과를 > 유효성 메세지 상태에 저장
+                // 필요없는 부분 줄여보기: onChange > 유효성을 체크 함수 > 유효성을 체크하고 메세지 저장 & setEmail
+                // email = {value: "", isValided: false, validatedMessage:""}
                 value={email}
                 required
                 aria-invalid={validEmail ? 'false' : 'true'}
@@ -291,44 +305,50 @@ const Register = () => {
                 onBlur={() => setEmailFocus(false)}
               />
               <label htmlFor="email">
+                {/* 멘토님 Q. 유효성 CSS 프롭이나 연산자 이용해서 변경하기를 만약 변경한다면.. 
+                {validEmail && <FontAwesomeIcon icon={faCircleCheck} />}
+                {!validEmail && <FontAwesomeIcon icon={faCircleXmark} />} */}
                 <FontAwesomeIcon icon={faCircleCheck} className={validEmail ? 'valid' : 'hide'} />
-                <FontAwesomeIcon icon={faCircleXmark} className={validEmail || !username ? 'hide' : 'invalid'} />
+                <FontAwesomeIcon icon={faCircleXmark} className={validEmail || !nickName ? `hide` : 'invalid'} />
               </label>
             </div>
             <p id="uidnote" className={emailFocus && email && !validEmail ? 'instructions' : 'offscreen'}>
               이메일 형태로 입력해주세요.
             </p>
+            <p>{emailMessage}</p>
+
             <div className="input-wrap">
               <input
                 placeholder="닉네임을 입력해주세요."
                 type="text"
-                id="username"
+                id="nickName"
                 ref={userRef}
                 autoComplete="off"
-                onChange={e => setUsername(e.target.value)}
-                value={username}
+                onChange={e => setNickname(e.target.value)}
+                value={nickName}
                 required
-                aria-invalid={validUsername ? 'false' : 'true'}
+                aria-invalid={validnickName ? 'false' : 'true'}
                 aria-describedby="uidnote"
                 onFocus={() => setUserFocus(true)}
                 onBlur={() => setUserFocus(false)}
               />
-              <label htmlFor="username">
-                <FontAwesomeIcon icon={faCircleCheck} className={validUsername ? 'valid' : 'hide'} />
-                <FontAwesomeIcon icon={faCircleXmark} className={validUsername || !username ? 'hide' : 'invalid'} />
+              <label htmlFor="nickName">
+                <FontAwesomeIcon icon={faCircleCheck} className={validnickName ? 'valid' : 'hide'} />
+                <FontAwesomeIcon icon={faCircleXmark} className={validnickName || !nickName ? 'hide' : 'invalid'} />
               </label>
             </div>
-            <p id="uidnote" className={userFocus && username && !validUsername ? 'instructions' : 'offscreen'}>
+            <p id="uidnote" className={userFocus && nickName && !validnickName ? 'instructions' : 'offscreen'}>
               4글자에서 24글자 이내로 입력해주세요. <br />
               문자, 숫자, 밑줄, 하이픈은 사용가능해요.
             </p>
+            <p>{nickNameMessage}</p>
             <div className="input-wrap">
               <input
                 placeholder="비밀번호를 입력해주세요."
                 type="password"
                 id="password"
-                onChange={e => setPwd(e.target.value)}
-                value={pwd}
+                onChange={e => setPassword(e.target.value)}
+                value={password}
                 required
                 aria-invalid={validPwd ? 'false' : 'true'}
                 aria-describedby="pwdnote"
@@ -337,7 +357,7 @@ const Register = () => {
               />
               <label htmlFor="password">
                 <FontAwesomeIcon icon={faCircleCheck} className={validPwd ? 'valid' : 'hide'} />
-                <FontAwesomeIcon icon={faCircleXmark} className={validPwd || !pwd ? 'hide' : 'invalid'} />
+                <FontAwesomeIcon icon={faCircleXmark} className={validPwd || !password ? 'hide' : 'invalid'} />
               </label>
             </div>
             <p id="pwdnote" className={pwdFocus && !validPwd ? 'instructions' : 'offscreen'}>
@@ -364,7 +384,7 @@ const Register = () => {
             <p id="confirmnote" className={matchFocus && !validMatch ? 'instructions' : 'offscreen'}>
               첫 번째 비밀번호와 일치하게 수정해주세요.🥹
             </p>
-            <button className={!validUsername || !validPwd || !validMatch ? 'btn-disabled' : 'btn-submit'}>
+            <button className={!validnickName || !validPwd || !validMatch ? 'btn-disabled' : 'btn-submit'}>
               회원가입
             </button>
             <div ref={errRef} className={errMsg ? 'errmsg' : 'offscreen'} aria-live="assertive">
