cac0b8bc70f71c3811b94b6299afbcd66ae566ea
gidoldory <gidol79@gmail.com>
2023-01-29 19:21:49 +0900

    ✨ feat: 추천장소 리스트/삭제기능 추가, toast 모달 위치 변경

--- client/src/pages/Mypage/Mypage.js
+++ client/src/pages/Mypage/Mypage.js
@@ -1,6 +1,8 @@
 import styled, { css } from 'styled-components'
 import { useState } from 'react'
 import { useNavigate, Outlet, useLocation } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart, faBookmark } from '@fortawesome/free-solid-svg-icons'
 
 import Nav from '../../components/Nav/Nav'
 import Header from '../../components/Header/Header'
@@ -46,20 +48,24 @@ const Menu = styled.li`
     `}
 `
 
+const FontAwesomeIconStyle = styled(FontAwesomeIcon)`
+  color: ${props => props.iconColor};
+  font-size: 18px;
+  vertical-align: top;
+`
+
 const Mypage = () => {
   useLoginCheck('redirect')
 
   // state, hook
   const navigate = useNavigate()
-  const location = useLocation()
-  const [selectedTab, setSelectedTab] = useState('북마크')
+  const { pathname } = useLocation()
+
+  const menuName = pathname.split('/')[2]
 
   // handle
-  const handleClickTab = e => {
-    const tabName = e.target.innerText
-    setSelectedTab(tabName)
-    if (tabName === '북마크') navigate('/mypage/bookmark')
-    if (tabName === '나의정보') navigate('/mypage/myinfo')
+  const handleClickTab = tabName => {
+    navigate('/mypage/' + tabName)
   }
 
   return (
@@ -69,10 +75,23 @@ const Mypage = () => {
         <Header />
         <Container>
           <TabMenu>
-            <Menu aria-label="북마크 탭 선택" selected={selectedTab === '북마크'} onClick={handleClickTab}>
-              북마크
+            <Menu
+              aria-label="북마크 탭 선택"
+              selected={menuName === 'bookmark'}
+              onClick={() => handleClickTab('bookmark')}
+            >
+              북마크&nbsp;
+              <FontAwesomeIconStyle icon={faBookmark} iconColor="green" />
             </Menu>
-            <Menu aria-label="나의정보 탭 선택" selected={selectedTab === '나의정보'} onClick={handleClickTab}>
+            <Menu aria-label="나의정보 탭 선택" selected={menuName === 'like'} onClick={() => handleClickTab('like')}>
+              추천장소&nbsp;
+              <FontAwesomeIconStyle icon={faHeart} iconColor="red" />
+            </Menu>
+            <Menu
+              aria-label="나의정보 탭 선택"
+              selected={menuName === 'myinfo'}
+              onClick={() => handleClickTab('myinfo')}
+            >
               나의정보
             </Menu>
           </TabMenu>
