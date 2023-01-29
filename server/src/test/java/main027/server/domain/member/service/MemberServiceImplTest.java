c746d3a6ff51e11e147daf4f153625701dd82f5a
wonyongg <kakaohwy@gmail.com>
2023-01-30 01:10:18 +0900

    test : memberService 테스트 코드2

--- server/src/test/java/main027/server/domain/member/service/MemberServiceImplTest.java
+++ server/src/test/java/main027/server/domain/member/service/MemberServiceImplTest.java
@@ -1,14 +1,11 @@
 package main027.server.domain.member.service;
 
 import main027.server.domain.member.entity.Member;
-import main027.server.domain.member.repository.MemberRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import java.time.LocalDateTime;
 
@@ -16,10 +13,8 @@ import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
 class MemberServiceImplTest {
-    @InjectMocks
-    private MemberServiceImpl memberService;
     @Mock
-    private MemberRepository memberRepository;
+    private MemberServiceImpl memberService;
 
 
     @Test
